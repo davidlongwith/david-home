26cf9ac03eec8e737a3699d624213a9107fee82d
David Longwith <david@davidlongwith.com>
2021-04-30 15:19:28 -0400

    Projects list save point

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,21 @@
 import React from "react";
 
 const Projects = ({ projectsData }) => (
-  <div>
-    <p>hello</p>
-    {console.log(projectsData)}
-  </div>
+  <section className="section-wrapper">
+    <h1 className="section-header">Projects</h1>
+    <div className="project-list">
+      {projectsData.length === 0 ? (
+        <p>Opps! No projects listed</p>
+      ) : (
+        projectsData.map((project) => (
+          <li key={project.name}>
+            project name: {project.name} project description:{" "}
+            {project.description}
+          </li>
+        ))
+      )}
+    </div>
+  </section>
 );
 
 export default Projects;
