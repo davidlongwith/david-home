7e2a9bd6c8d89614ca5ad87118585d3b6aae228e
David Longwith <david@davidlongwith.com>
2022-08-25 16:56:41 -0400

    update About me picture content

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,12 +1,12 @@
 import React from "react";
-import avatar from "../images/image.jpg";
+import AboutMePic from "../images/600x600_lite.jpg";
 import "../css/aboutme.css";
 
 const AboutMe = () => (
   <section className="section-wrapper">
     <h1 className="section-header">About me</h1>
     <div className="about-me-content">
-      <img src={avatar} alt="" />
+      <img src={AboutMePic} alt="" />
       <p>
         My name is David Longwith. I develop web sites and applications at all
         stages from concept to deployment. My expertise is primarly focused on
